12961f04c897561d215cca0ecb11a78ec9a15fae
jvm-kbh <elasticsearch@kakao.com>
2023-11-10 12:54:36 +0900

    first commit

---
+++ src/main/java/me/kbh/snsmodeling/SnsModelingApplication.java
@@ -0,0 +1,13 @@
+package me.kbh.snsmodeling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsModelingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SnsModelingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/kbh/snsmodeling/SnsModelingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kbh.snsmodeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsModelingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
